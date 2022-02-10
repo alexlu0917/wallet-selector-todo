582478c5aa98fd46ba97c1b7be1d880c38d9e7fa
alexlu0917 <alexlu0917.dev@gmail.com>
2022-02-10 00:31:23 +0000

    create the page component

---
+++ src/pages/WalletConnector.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { injected, frame, trezor } from "../utils/connector";
+
+const WalletConnector = () => {
+  return <>something</>;
+};
+
+export default WalletConnector;
