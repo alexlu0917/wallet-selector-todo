8dc0098d560beb2b0963c84292973566d10f9e81
alexlu0917 <alexlu0917.dev@gmail.com>
2022-02-10 00:31:04 +0000

    create the app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import WalletConnector from "./pages/WalletConnector";
+
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider, connector) {
+  return new Web3Provider(provider);
+}
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WalletConnector />} />
+        </Routes>
+      </BrowserRouter>
+    </Web3ReactProvider>
   );
 }
 
