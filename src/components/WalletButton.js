fbb8284c3d2041b4aaa1609834ef898a84293f28
alexlu0917 <alexlu0917.dev@gmail.com>
2022-02-22 01:25:21 +0000

    add the connect function for metamask

--- src/components/WalletButton.js
+++ src/components/WalletButton.js
@@ -1,32 +1,9 @@
 import React from "react";
-import Web3 from "web3";
-import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 
-const WalletButton = ({ icon, walletName, connector = "" }) => {
-  const context = useWeb3React();
-  const {
-    // connector,
-    library,
-    chainId,
-    account,
-    activate,
-    deactivate,
-    active,
-    error,
-  } = context;
-
-  const handleConnect = async () => {
-    console.log('click', 'metamask')
-    // read-only
-    // let web3 = new Web3.providers.HttpProvider(process.env.API_ENDPOINT);
-    await activate(connector);
-    let { provider } = await connector.activate();
-    // // signer
-    let web3 = new Web3(provider);
-  };
+const WalletButton = ({ icon, walletName, handleConnect, connector = "" }) => {
 
   return (
-    <div className="wallet-button col" onClick={(e) => handleConnect()}>
+    <div className="wallet-button col-md-6" onClick={(e) => handleConnect(connector, walletName)}>
       <span>
         <img src={icon} />
       </span>
--- src/pages/WalletConnector.js
+++ src/pages/WalletConnector.js
@@ -1,40 +1,171 @@
-import React from "react";
-import { injected, frame } from "../utils/connector";
+import React, { useEffect, useState } from "react";
+import Web3 from "web3";
+import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
+} from "@web3-react/injected-connector";
+import { UserRejectedRequestError as UserRejectedRequestErrorFrame } from "@web3-react/frame-connector";
+import Spinner from "react-bootstrap/Spinner";
+
+import { connectorsData } from "../utils/connector";
+// import { useEagerConnect, useInactiveListener } from "../utils/hooks";
+
 import bagIcon from "../assets/icon/bag_icon.png";
-import metamaskIcon from "../assets/icon/metamask_icon.png";
-import trezerIcon from "../assets/icon/Trezer.png";
-import ledgerIcon from "../assets/icon/ledger.png";
-import frameIcon from "../assets/icon/frame_icon.png";
-import WalletButton from '../components/WalletButton';
+import WalletButton from "../components/WalletButton";
 
 const WalletConnector = () => {
+  const context = useWeb3React();
+  const {
+    connector,
+    library,
+    chainId,
+    account,
+    activate,
+    deactivate,
+    active,
+    error,
+  } = context;
+
+  const [currentConnector, setCurrentConnector] = useState();
+
+  useEffect(() => {
+    if (currentConnector && currentConnector === connector) {
+      setCurrentConnector(undefined);
+    }
+  }, [connector, currentConnector]);
+
+  // const triedEager = useEagerConnect();
+
+  // useInactiveListener(!triedEager || !!currentConnector);
+
+  const [provider, setProvider] = useState();
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+
+  const handleConnect = async (selectedConnector, name) => {
+    setLoading(true);
+
+    // if (connector && connector === selectedConnector) {
+    //   console.log(active, "acitve");
+    //   if (active) {
+    //     alert("next page");
+    //     setLoading(false);
+    //   }
+    // }
+    // if (!selectedConnector) return;
+
+    try {
+      await activate(selectedConnector);
+      let { provider } = await selectedConnector.activate();
+      setProvider(provider);
+      setName(name);
+      setCurrentConnector(selectedConnector);
+      // let web3 = new Web3(provider);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      if (error.code === -32002) {
+        alert("Please check your Wallet. Need to connect your wallet.");
+      }
+      if (error instanceof NoEthereumProviderError) {
+        return "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.";
+      } else if (error instanceof UnsupportedChainIdError) {
+        return "You're connected to an unsupported network.";
+      } else if (
+        error instanceof UserRejectedRequestErrorInjected ||
+        error instanceof UserRejectedRequestErrorFrame
+      ) {
+        return "Please authorize this website to access your Ethereum account.";
+      } else {
+        return "An unknown error occurred. Check the console for more details.";
+      }
+    }
+  };
+
+  useEffect(() => {
+    const { ethereum } = window;
+    if (ethereum && ethereum.on && !active && !error ) {
+      const handleChainChanged = chainId => {
+        setLoading(false);
+        console.log("chainChanged", chainId);
+        activate(currentConnector);
+      };
+
+      const handleAccountsChanged = accounts => {
+        setLoading(false);
+        console.log("accountsChanged", accounts);
+        if (accounts.length > 0) {
+          activate(currentConnector);
+        }
+      };
+
+      const handleNetworkChanged = networkId => {
+        setLoading(false);
+        console.log("networkChanged", networkId);
+        activate(currentConnector);
+      };
+
+      const handleConnectorChanged = () => {
+        setLoading(false);
+        console.log("Web3ReactUpdate", name);
+        activate(currentConnector);
+      };
+
+      // ethereum.on("chainChanged", handleChainChanged);
+      ethereum.on("accountsChanged", handleAccountsChanged);
+      // ethereum.on("networkChanged", handleNetworkChanged);
+      ethereum.on("Web3ReactUpdate", handleConnectorChanged);
+
+      return () => {
+        if (ethereum.removeListener) {
+          // ethereum.removeListener("chainChanged", handleChainChanged);
+          ethereum.removeListener("accountsChanged", handleAccountsChanged);
+          // ethereum.removeListener("networkChanged", handleNetworkChanged);
+          ethereum.removeListener("Web3ReactUpdate", handleConnectorChanged);
+        }
+      };
+    }
+  }, [active, error, activate]);
+
   return (
     <div className="main-container">
       <div className="wallet-connector main-card">
-        <div>
-          <div className="select-wallet">
-            <span>
-              <img src={bagIcon} />
-            </span>
-            <h2>Select a Wallet</h2>
-          </div>
-        </div>
-        <div className="pt-20">
-          <h3>Please Select a wallet to connect to this dapp:</h3>
-          <div className="wallet-container">
-            <div className="row">
-                <WalletButton icon={metamaskIcon} walletName='MetaMask' connector={injected} />
-                <WalletButton icon={frameIcon} walletName='Frame' connector={frame}/>
+        {!loading && (
+          <>
+            <div>
+              <div className="select-wallet">
+                <span>
+                  <img src={bagIcon} />
+                </span>
+                <h2>Select a Wallet</h2>
+              </div>
             </div>
-            <div className="row">
-                <WalletButton icon={trezerIcon} walletName='Trezer' />
-                <WalletButton icon={ledgerIcon} walletName='Ledger' />
+            <div className="pt-20 w-100">
+              <h3>Please Select a wallet to connect to this dapp:</h3>
+              <div className="wallet-container w-100 d-flex flex-wrap">
+                {Object.values(connectorsData).map((item, index) => (
+                  <WalletButton
+                    icon={item.icon}
+                    walletName={item.name}
+                    key={index}
+                    connector={item.connector}
+                    handleConnect={handleConnect}
+                  />
+                ))}
+              </div>
             </div>
+            <div className="pt-20 d-flex justify-content-center">
+              <button className="btn rounded-border">Show More</button>
+            </div>
+          </>
+        )}
+        {loading && (
+          <div className="d-flex justify-content-center align-items-center">
+            <Spinner animation="border" role="status"></Spinner>
+            <span className="m-3">Connecting {name ? name : ""}</span>
           </div>
-        </div>
-        <div className="pt-20 d-flex justify-content-center">
-            <button className="btn rounded-border">Show More</button>
-        </div>
+        )}
       </div>
     </div>
   );
